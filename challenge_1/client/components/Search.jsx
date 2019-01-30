6a14be615132e3f255d1dc4a1f66a67d7bf880eb
Jessica <jessicarhodson@gmail.com>
2019-01-30 11:30:15 -0800

    Enable search by keyword

--- challenge_1/client/components/Search.jsx
+++ challenge_1/client/components/Search.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Search = (props) => {
   return (
-    <form>
+    <form onSubmit={props.searchByKeyword}>
       Find Historical Records:
-      <input type='text' name='keyword'></input>
+      <input type='text' name='keyword' onChange={props.handleSearchInput.bind(this)}></input>
       <button type='submit'>Search by Keyword</button>
     </form>
   )
