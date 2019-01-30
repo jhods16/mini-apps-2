916b3a8498e098940f1dca48d2fc6acc613bcd05
Jessica <jessicarhodson@gmail.com>
2019-01-30 10:32:08 -0800

    Outline client structure in App.jsx

--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -1,2 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Search from './components/Search.jsx';
+import DataViewer from './components/DataViewer.jsx';
 
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
 
+  render() {
+    return (
+      <div>
+        <h2>Historical Data Viewer</h2>
+        <Search />
+        <DataViewer />
+      </div>
+    )
+  }
+
+}
+
+ReactDOM.render(<App/>, document.getElementById('app'))
