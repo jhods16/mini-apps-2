f5835e9ff8c0313c24ee95b3c564aa2c55f23b77
Jessica <jessicarhodson@gmail.com>
2019-01-30 17:12:04 -0800

    Add comments to explain conditional logic

--- challenge_1/client/components/DataEntry.jsx
+++ challenge_1/client/components/DataEntry.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 
 const DataEntry = (props) => {
+  // if in 'editing' mode, this entry will have different values in the following places
+  //  1. contentEditable will be set to 'true'
+  //  2. the button click event will call the 'handleSave' function rather than the 'handleEdit' function
+  //  3. the button text will read 'Save' rather than 'Edit'
   return (
     <tr>
-      <td>{props.date}</td>
-      <td>{props.description}</td>
+      <td contentEditable={props.editing} onInput={props.handleInput.bind(this)}>{props.date}</td>
+      <td contentEditable={props.editing}>{props.description}</td>
+      <td>
+        <button onClick={props.editing ? props.handleSave.bind(this) : props.handleEdit.bind(this, props)}>
+          {props.editing ? 'Save' : 'Edit'}
+        </button>
+      </td>
     </tr>
   )
 }
