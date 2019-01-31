2783e38f0af3181af8a5e3211350b16a0542da70
Jessica <jessicarhodson@gmail.com>
2019-01-30 17:11:04 -0800

    Clean up formatting in DataViewer.jsx

--- challenge_1/client/components/DataViewer.jsx
+++ challenge_1/client/components/DataViewer.jsx
@@ -11,7 +11,18 @@ const DataViewer = (props) => {
             <th>Date</th>
             <th>Description</th>
           </tr>
-          {props.data.map((entry, i) => <DataEntry key={i} date={entry.date} description={entry.description}/>)}
+          {props.data.map((entry, i) => 
+            <DataEntry 
+              key={i} 
+              date={entry.date} 
+              description={entry.description} 
+              handleEdit={props.handleEdit}
+              handleInput={props.handleInput}
+              handleSave={props.handleSave}
+              // if the current entry being rendered matches the entry being edited, set the 'editing' prop to true
+              editing={props.editing.description === entry.description ? true : false} 
+              />
+            )}
         </tbody>
       </table>
     </div>
