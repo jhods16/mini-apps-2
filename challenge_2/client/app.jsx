01e3dd8ce5ab25de35cddafa05704fe75729e3a3
Jessica <jessicarhodson@gmail.com>
2019-01-31 15:18:30 -0800

    Route request for data to CoinDesk API, return results

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -1,14 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
+import BPIChart from './components/Chart.jsx';
+import chartOptions from './chart.config.js';
 
 class App extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      options: chartOptions
+    }
+  }
+
+  componentDidMount() {
+    axios.get('/data')
+      .then(results => {
+        this.setState({ 
+          options: {
+            type: this.state.options.type,
+            data: {
+              labels: Object.keys(results.data),
+              datasets: [{
+                label: this.state.options.data.datasets[0].label,
+                data: Object.values(results.data),
+                backgroundColor: this.state.options.data.datasets[0].backgroundColor,
+                borderColor: this.state.options.data.datasets[0].borderColor,
+              }]
+            },
+            options: this.state.options.options,
+          }
+        })
+      })
+      .catch(err => console.error(err));
   }
   
   render() {
     return (
-      <div>Look, an App!</div>
+      <div>
+        <BPIChart chartOptions={this.state.options}/>
+        <p>Powered by CoinDesk</p>
+      </div>
     )
   }
 }
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -3,9 +3,18 @@ let app = express();
 let port = 3000;
 let bodyParser = require('body-parser');
 let path = require('path');
+let axios = require ('axios');
 
 app.use(express.static(path.join(__dirname, './public'))); 
 app.use(bodyParser.json());
+
+app.get('/data', (req, res) => {
+  axios.get('https://api.coindesk.com/v1/bpi/historical/close.json')
+    .then(results => {
+      res.send(results.data.bpi);
+    })
+    .catch(err => console.error(err));
+})
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
