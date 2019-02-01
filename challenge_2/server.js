d28cdbb331f89293450303c1ee313d460ce25a41
Jessica <jessicarhodson@gmail.com>
2019-01-31 17:14:11 -0800

    Implement cache-aside strategy with Redis

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -3,20 +3,12 @@ let app = express();
 let port = 3000;
 let bodyParser = require('body-parser');
 let path = require('path');
-let axios = require ('axios');
+let controllers = require('./controllers.js')
 
 app.use(express.static(path.join(__dirname, './public'))); 
 app.use(bodyParser.json());
 
-app.get('/data', (req, res) => {
-  axios.get('https://api.coindesk.com/v1/bpi/historical/close.json', {
-    params: req.query
-  })
-    .then(results => {
-      res.send(results.data.bpi);
-    })
-    .catch(err => res.send(err));
-})
+app.get('/data', controllers.getCachedData)
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
