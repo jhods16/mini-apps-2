f7938f9d3be957738a54f62a9221df85a246d571
Jessica <jessicarhodson@gmail.com>
2019-02-01 14:49:40 -0800

    Initial setup - webpack, server, react

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,12 @@
+let express = require('express');
+let app = express();
+let port = 3000;
+let path = require('path')
+
+app.use(express.static(path.join(__dirname, './public')));
+
+app.listen(port, () => {
+  console.log(`Listening on ${port}!`)
+})
 
 
