04ece97af495bf8c348a2d85c929bc13b5851eb0
Jessica <jessicarhodson@gmail.com>
2019-01-31 16:35:33 -0800

    Refactor chart update method, maintain state in chart component
    Made use of Chartjs API to update the chart rather than re-creating a chart on componentDidUpdate, as I was doing before. The result is a much cleaner state update flow in App.jsx and enables continual updates after form submission.

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -3,41 +3,63 @@ import ReactDOM from 'react-dom';
 import axios from 'axios';
 import BPIChart from './components/Chart.jsx';
 import chartOptions from './chart.config.js';
+import DatePicker from './components/DatePicker.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      options: chartOptions
+      // options: chartOptions,
+      labels: [],
+      data: [],
+      start: '',
+      end: '',     
     }
+    this.updateChart = this.updateChart.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount() {
-    axios.get('/data')
-      .then(results => {
-        this.setState({ 
-          options: {
-            type: this.state.options.type,
-            data: {
-              labels: Object.keys(results.data),
-              datasets: [{
-                label: this.state.options.data.datasets[0].label,
-                data: Object.values(results.data),
-                backgroundColor: this.state.options.data.datasets[0].backgroundColor,
-                borderColor: this.state.options.data.datasets[0].borderColor,
-              }]
-            },
-            options: this.state.options.options,
-          }
-        })
-      })
-      .catch(err => console.error(err));
+    this.updateChart({});
   }
   
+  updateChart(query) {
+    axios.get('/data', query)
+      .then(results => {
+        this.setState({
+          labels: Object.keys(results.data),
+          data: Object.values(results.data),
+        })
+      })
+      .catch(err => console.error(err));  
+  }
+  
+  handleChange(e) {
+    let name = e.target.name;
+    let value = e.target.value;
+
+    this.setState({
+      [name]: value  
+    })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    let query = { 
+      params: {
+        start: this.state.start,
+        end: this.state.end
+      }
+    }
+    this.updateChart(query);
+  }
+
   render() {
     return (
       <div>
-        <BPIChart chartOptions={this.state.options}/>
+        <DatePicker handleChange={this.handleChange} handleSubmit={this.handleSubmit} />
+        <BPIChart labels={this.state.labels} data={this.state.data}/>
         <p>Powered by CoinDesk</p>
       </div>
     )
--- challenge_2/client/components/Chart.jsx
+++ challenge_2/client/components/Chart.jsx
@@ -1,16 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Chart from 'chart.js';
+import chartOptions from '../chart.config.js';
 
 
 class BPIChart extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      chart: {}
+    }
+    this.buildChart = this.buildChart.bind(this);
+  }
+
+  componentDidMount() {
+    this.buildChart()
+  }
+
+  buildChart() {
+    let ctx = ReactDOM.findDOMNode(this.refs.bpichart) 
+    let bpiChart = new Chart(ctx, chartOptions);
+    
+    this.setState({
+      chart: bpiChart
+    })
   }
 
   componentDidUpdate() {
-    let ctx = ReactDOM.findDOMNode(this.refs.bpichart) 
-    let bpiChart = new Chart(ctx, this.props.chartOptions); 
+    let currentChart = this.state.chart;
+    currentChart.data.labels = this.props.labels;
+    currentChart.data.datasets[0].data = this.props.data;
+    currentChart.update();
   }
 
   render() {
@@ -21,3 +41,5 @@ class BPIChart extends React.Component {
 }
 
 export default BPIChart;
+
+
