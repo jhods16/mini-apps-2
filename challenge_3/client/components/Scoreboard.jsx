4e3f0014675f6017ec8bb2e365c20c48b452c992
Jessica <jessicarhodson@gmail.com>
2019-02-01 18:47:11 -0800

    Redesign frame/scoreboard

--- challenge_3/client/components/Scoreboard.jsx
+++ challenge_3/client/components/Scoreboard.jsx
@@ -4,16 +4,12 @@ import Frame from './Frame.jsx';
 let Scoreboard = (props) => {
   return (
     <div>
-      <Frame key='1'/>
-      <Frame key='2'/>
-      <Frame key='3'/>
-      <Frame key='4'/>
-      <Frame key='5'/>
-      <Frame key='6'/>
-      <Frame key='7'/>
-      <Frame key='8'/>
-      <Frame key='9'/>
-      <Frame key='10'/>
+      {props.plays.map((play, i) => {
+        return <Frame 
+                key={i} 
+                bowlScore={play.bowlScore} 
+                totalScore={play.totalScore} />
+      })}
     </div>
   )
 }
