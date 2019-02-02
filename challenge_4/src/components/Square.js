66dc69e8c59f030e1fb7e2a0b5fcd1dc0ebc0b59
Jessica <jessicarhodson@gmail.com>
2019-02-02 15:57:08 -0800

    Set visibility of square value to hidden

--- challenge_4/src/components/Square.js
+++ challenge_4/src/components/Square.js
@@ -5,7 +5,8 @@ const Square = ({x, y, val, handleSquareClick}) => {
     <div 
       style={{height: '20px', width: '20px', border: '1px solid black', display: 'inline-block'}}
       onClick={handleSquareClick.bind(this, x, y, 1)}
-      >{val}
+      >
+      <div style={{visibility: 'hidden'}}>{val}</div>
     </div> 
   )
 }
